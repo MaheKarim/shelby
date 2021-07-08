478390ec54121c8c80ddf8153aabd1d82ed75ce7
Mahe Karim <mahekarim@gmail.com>
2021-07-09 03:02:57 +0600

    Income Fully Okay (#2)

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\IncomeController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
@@ -22,3 +23,12 @@ Route::get('login', [AuthController::class,'loginPage'])->name('auth.login.page'
 Route::post('login', [AuthController::class,'login'])->name('auth.login');
 Route::get('logout', [AuthController::class,'logout'])->name('auth.logout');
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard')->middleware('auth');
+
+Route::prefix('/dashboard')->group(function (){
+    Route::get('/incomes', [IncomeController::class, 'index'])->name('incomes.index');
+    Route::get('/incomes/create', [IncomeController::class, 'create'])->name('incomes.create');
+    Route::post('/incomes', [IncomeController::class, 'store'])->name('incomes.store');
+    Route::get('/incomes/{id}/edit', [IncomeController::class, 'edit'])->name('incomes.edit');
+    Route::put('/incomes/{id}', [IncomeController::class, 'update'])->name('incomes.update');
+    Route::get('/incomes/{id}', [IncomeController::class, 'delete'])->name('incomes.delete');
+});
