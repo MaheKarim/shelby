7c3e624ab2d7d71cef255b83ddc69dbd5afc91cc
Mahe Karim <mahekarim@deligram.com>
2021-07-13 23:46:12 +0600

    Items CRUD Sidebar Added

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -15,6 +15,9 @@
             <li class="active">
                 <a href="{{ route('expenses.index') }}"><i class="fas fa-lemon"></i><span>Expense</span></a>
             </li>
+            <li class="active">
+                <a href="{{ route('items.index') }}"><i class="fas fa-code"></i><span>Items</span></a>
+            </li>
         </ul>
     </aside>
 </div>
